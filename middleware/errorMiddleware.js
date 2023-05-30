7af82921b67b18f7fb53f2f9897115e469d0cf55
zaryab <zaryabdeveloper49@gmail.com>
2023-05-30 10:04:46 +0500

    error handling bug fixed

--- middleware/errorMiddleware.js
+++ middleware/errorMiddleware.js
@@ -6,12 +6,16 @@ const notFound = (req, res, next) => {
 };
 
 const errorHandler = (err, req, res, next) => {
-  const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
+  let statusCode = res.statusCode === 200 ? 500 : res.statusCode;
+  let message = err.message;
+
+  if(err.name === 'CastError' && err.kind === 'ObjectId'){
+    message ='resource not found';
+    statusCode = 404
+  }
   res.status(statusCode);
   res.json({
-    message: res.json({
-      error: `Unknown Internal Server Error ${err.message}`,
-    }),
+    message,
     stack: process.env.NODE_ENV === "production" ? null : err.stack,
   });
 };
