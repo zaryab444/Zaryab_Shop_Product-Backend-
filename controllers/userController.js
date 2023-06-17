c1f5182e94dc2485795947e580c744727230ddda
zaryab <zaryabdeveloper49@gmail.com>
2023-06-17 23:23:57 +0500

    update user profile api work

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,26 +1,27 @@
 const asyncHandler = require("express-async-handler");
 const User = require("../data/models/userModel");
-const generateToken = require ("../utils/generateToken");
-
+const generateToken = require("../utils/generateToken");
 
 //@desc Login user
 //route POST /api/users/login
 //@access Public
 //api http://localhost:5000/api/users/login
 const authUser = asyncHandler(async (req, res) => {
-  const {email, password} = req.body;
-  const user = await User.findOne({email});
-  if(user && (await user.matchPassword(password))){
-   generateToken(res,user._id);
-    res.json({
-      _id: user._id,
-      name: user.name,
-      email: user.email,
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
+  if (user && (await user.matchPassword(password))) {
+    generateToken(res, user._id);
+    res.status(200).json({
+      // _id: user._id,
+      // name: user.name,
+      // email: user.email,
+      // isAdmin: user.isAdmin,
+      message: "User Login Successfully",
       isAdmin: user.isAdmin,
-    })
+    });
   } else {
     res.status(401);
-    throw new Error('Invalid email or password');
+    throw new Error("Invalid email or password");
   }
   res.send("auth user");
 });
@@ -30,33 +31,33 @@ const authUser = asyncHandler(async (req, res) => {
 //@access Public
 //api http://localhost:5000/api/users
 const registerUser = asyncHandler(async (req, res) => {
-const {name,email,password} = req.body;
+  const { name, email, password } = req.body;
 
-const userExist = await User.findOne({email});
+  const userExist = await User.findOne({ email });
 
-if(userExist){
-  res.status(400);
-  throw new Error('User already exists');
-}
+  if (userExist) {
+    res.status(400);
+    throw new Error("User already exists");
+  }
 
-const user = await User.create({
-  name,
-  email,
-  password
-});
+  const user = await User.create({
+    name,
+    email,
+    password,
+  });
 
-if(user){
-  generateToken(res,user._id);
-  res.status(200).json({
-    _id: user._id,
-    name: user.name,
-    email: user.email,
-    isAdmin: user.isAdmin
-  })
-} else {
-  res.status(400);
-  throw new Error('Invalid user data');
-}
+  if (user) {
+    generateToken(res, user._id);
+    res.status(200).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+    });
+  } else {
+    res.status(400);
+    throw new Error("Invalid user data");
+  }
 });
 
 //@desc Logout user / clear cookie
@@ -64,13 +65,13 @@ if(user){
 //@access Private
 //api http://localhost:5000/api/users/logout
 const logoutUser = asyncHandler(async (req, res) => {
- res.cookie('jwt', '', {
-  httpOnly: true,
-  expires: new Date(0)
- });
- res.status(200).json({
-  message: 'Logged out successfully'
- });
+  res.cookie("jwt", "", {
+    httpOnly: true,
+    expires: new Date(0),
+  });
+  res.status(200).json({
+    message: "Logged out successfully",
+  });
 });
 
 //@desc Get user profile
@@ -78,7 +79,19 @@ const logoutUser = asyncHandler(async (req, res) => {
 //@access Private
 //api http://localhost:5000/api/users/profile
 const getUserProfile = asyncHandler(async (req, res) => {
-  res.send("get user profile");
+  const user = await User.findById(req.user._id);
+
+  if (user) {
+    res.status(200).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+    });
+  } else {
+    res.status(404);
+    throw new Error("User not found");
+  }
 });
 
 //@desc Update user profile
@@ -86,7 +99,30 @@ const getUserProfile = asyncHandler(async (req, res) => {
 //@access Private
 //api http://localhost:5000/api/users/profile
 const updateUserProfile = asyncHandler(async (req, res) => {
-  res.send("update user profile");
+  const user = await User.findById(req.user._id);
+
+  if (user) {
+    user.name = req.body.name || user.name;
+    user.email = req.body.email || user.email;
+
+    /**
+     * the reason of doing this because password 
+    is hash in store in database
+     * */
+    if (req.body.password) {
+      user.password = req.body.password;
+    }
+    const updateUser = await user.save();
+    res.status(200).json({
+      _id: updateUser._id,
+      name: updateUsera.name,
+      email: updateUser.email,
+      isAdmin: updateUser.isAdmin
+    });
+  } else {
+    res.status(404);
+    throw new Error('User not found');
+  }
 });
 
 //@desc Get users
@@ -130,5 +166,5 @@ module.exports = {
   getUsers,
   deleteUser,
   getUserByID,
-  updateUser
+  updateUser,
 };
