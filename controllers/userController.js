d8404a94cfa44022860a57658b7c49930c461d8d
zaryab444 <zaryabsohail444@gmail.com>
2023-07-04 19:17:51 +0500

    user controller work

--- controllers/userController.js
+++ controllers/userController.js
@@ -143,7 +143,8 @@ const updateUserProfile = asyncHandler(async (req, res) => {
 //@access Private/Admin
 //api http://localhost:5000/api/users
 const getUsers = asyncHandler(async (req, res) => {
-  res.send("get users");
+ const users = await User.find({});
+ res.status(200).json(users);
 });
 
 //@desc DELETE users
@@ -151,7 +152,20 @@ const getUsers = asyncHandler(async (req, res) => {
 //@access Private/Admin
 //api http://localhost:5000/api/users/1
 const deleteUser = asyncHandler(async (req, res) => {
-  res.send("delete users");
+ const user = await User.findById(req.params.id);
+
+ if(user){
+  if(user.isAdmin){
+    res.status(400);
+    throw new Error('Cannot admin user');
+  }
+  await User.deleteOne({_id: user._id})
+  res.status(200).json({message: 'User deleted successfully'});
+ }
+ else {
+  res.status(404);
+  throw new Error('User not found');
+ }
 });
 
 //@desc GET user by ID
@@ -159,7 +173,14 @@ const deleteUser = asyncHandler(async (req, res) => {
 //@access Private/Admin
 //api http://localhost:5000/api/users/1
 const getUserByID = asyncHandler(async (req, res) => {
-  res.send("get user by id");
+const user = await User.findById(req.params.id).select('-password');
+
+if(user){
+  res.status(200).json(user);
+} else {
+  res.status(404);
+  throw new Error('User not found');
+}
 });
 
 //@desc Update ser
@@ -167,7 +188,24 @@ const getUserByID = asyncHandler(async (req, res) => {
 //@access Private/Admin
 //http://localhost:5000/api/users/1
 const updateUser = asyncHandler(async (req, res) => {
-  res.send("update user");
+  const user = await User.findById(req.params.id);
+  
+  if(user){
+    user.name = req.body.name || user.name;
+    user.email = req.body.email || user.email;
+    user.isAdmin = Boolean(req.body.isAdmin);
+
+    const updateUser = await user.save();
+    res.status(200).json({
+      _id:updateUser._id,
+      name: updateUser.name,
+      email: updateUser.email,
+      isAdmin: updateUser.isAdmin
+    });
+  } else {
+    res.status(404);
+    throw new Error('User not found');
+  }
 });
 
 module.exports = {
--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -5,11 +5,12 @@ const {
   getProductById,
   createProduct,
   updateProduct,
+  deleteProduct,
 } = require("../controllers/productController");
 const {protect, admin} = require ("../middleware/authMiddleware");
 
 
 router.route("/").get(getProducts).post(protect, admin, createProduct);
-router.route("/:id").get(getProductById).put(protect, admin,updateProduct);
+router.route("/:id").get(getProductById).put(protect, admin,updateProduct).delete(protect, admin, deleteProduct);
 
 module.exports = router;
